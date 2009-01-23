3d2315b3481f4fe3ff0df3aa7572eefda0137406
matthewdpotts <matthewdpotts@7cf02e72-453a-0410-a42e-33068982fbc4>
2009-01-23 17:59:24 +0000

    git-svn-id: http://modelframe.googlecode.com/svn/trunk@50 7cf02e72-453a-0410-a42e-33068982fbc4

---
+++ modelframe/model/pollination/PollenGrain.java
@@ -0,0 +1,14 @@
+package model.pollination;
+
+public class PollenGrain {
+	
+	public int plant_type, plant_id;
+	
+	public PollenGrain( int plant_type, int plant_id) {
+		this.plant_type = plant_type;
+		this.plant_id = plant_id;
+
+	}
+
+
+}
